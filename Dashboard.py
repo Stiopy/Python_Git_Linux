4e41f89590de6c50aebe0cdd0c300ce1d3742f96
Stiopy <119943177+Stiopy@users.noreply.github.com>
2025-04-05 21:39:17 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -79,4 +79,4 @@ def update_dashboard(n):
 
 # Lancer le serveur
 if __name__ == "__main__":
-    app.run_server(debug=True, host = '0.0.0.0')
+    app.run_server(debug=True, host = '0.0.0.0', port = 8050)
